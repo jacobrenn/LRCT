b3c1b27d04fb40b0ab1bd233923df2819bcaac1d
Jacob <jwrenn4@outlook.com>
2019-11-27 08:06:43 -0500

    removing AlreadyFitException (per scikit guidelines, Estimators should be available to be fit multiple times)

--- LRCT/Exceptions.py
+++ LRCT/Exceptions.py
@@ -1,11 +1,3 @@
-class AlreadyFitError(Exception):
-    '''An Exception to be thrown when trying to fit an object that was already fit'''
-    def __init__(self, *args, **kwargs):
-        if not any([args, kwargs]):
-            super().__init__('Object already fit')
-        else:
-            super().__init__(*args, **kwargs)
-
 class NotFitError(Exception):
     '''An Exception to be thrown when an object is not fit'''
     def __init__(self, *args, **kwargs):
