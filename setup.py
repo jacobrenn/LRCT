47e3421736dad091ab94d5a87e609f6676a59884
Ubuntu <stocknet@StocknetVM.lwsnq3pkcwbefncdnawlzb0njf.bx.internal.cloudapp.net>
2020-05-30 21:47:09 -0400

    moving requirements to requirements file instead of installing dependencies automatically

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
-import distutils.core
+from distutils.core import setup
 from LRCT import __version__
 
-distutils.core.setup(
+setup(
     name='LRCT',
     version=__version__,
     packages=['LRCT'],
     author='Jacob Renn',
     author_email='jwrenn4@outlook.com',
     description='Python package for implementing Linear Regression Classification Trees',
-    long_description=open('README.md').read(),
-    requires=['scikit-learn', 'pandas', 'numpy']
+    long_description=open('README.md').read()
 )
