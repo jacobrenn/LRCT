b7495bb4a6ca3313e44e9d255d6174d2c6f9a077
Jacob Renn <jwrenn4@outlook.com>
2021-01-29 17:37:17 -0500

    adding utils

---
+++ experiments/exp_utils.py
@@ -0,0 +1,26 @@
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score, classification_report
+
+def model_report(
+    model,
+    x_test,
+    y_test,
+    binary = True
+):
+    preds = model.predict(x_test)
+    
+    acc = accuracy_score(y_test, preds)
+    conf_mat = confusion_matrix(y_test, preds)
+    report = classification_report(y_test, preds)
+
+    print(f'Accuracy Score: {acc}')
+
+    if binary:
+        probs = model.predict_proba(x_test)[:, 1]
+        auc = roc_auc_score(y_test, probs)
+        print(f'AUC: {auc}')
+
+    print('Confusion Matrix:')
+    print(conf_mat)
+
+    print('Classification Report:')
+    print(classification_report(y_test, preds))
