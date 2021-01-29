a1fc3b341d1cfadc4850708d63141f86a8cca9db
Jacob Renn <jwrenn4@outlook.com>
2021-01-29 15:09:51 -0500

    adding updated header

--- experiments/set_1_2VarLinear.py
+++ experiments/set_1_2VarLinear.py
@@ -2,3 +2,11 @@
 
 # 50% of data for training, 20% for validation, 30% for testing
 # Model types to choose: LRCT, OC1, CART
+
+# Report: accuracy, confusion matrix, per-class and average F1, AUC
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from LRCT import LRCTree
+from sklearn.tree import DecisionTreeClassifier
+import oc1
