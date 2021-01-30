c1eb15dec47782c35ac820a3254aadee146ffa66
Jacob Renn <jwrenn4@outlook.com>
2021-01-30 09:20:18 -0500

    updating experiment

--- experiments/set_1_2VarLinear.py
+++ experiments/set_1_2VarLinear.py
@@ -9,3 +9,9 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from LRCT import LRCTree
 from sklearn.tree import DecisionTreeClassifier
+
+
+if __name__ == '__main__':
+    # Generate the data, domain for x0 is [0, 14], domain for x1 is [0, 8]
+    two_var_x = np.random.random((1000, 2))
+    two_var_x[:, 0] = two_var_x[:, 0] * 14
