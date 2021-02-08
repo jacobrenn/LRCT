dcca32a64f86f64922dc0f883391403de85ddc75
Jacob Renn <jwrenn4@outlook.com>
2021-02-08 13:07:01 +0000

    updating exp 8 script - INCOMPLETE

--- experiments/set_8_5VarPolynomialNoise.py
+++ experiments/set_8_5VarPolynomialNoise.py
@@ -27,30 +27,37 @@ if __name__ == '__main__':
     # Create train, val, test sets
     x_train_val, x_test, y_train_val, y_test = train_test_split(two_var_x, target, test_size = 0.3, random_state = 134456)
     x_train, x_val, y_train, y_val = train_test_split(x_train_val, y_train_val, test_size = 0.4, random_state = 89582)
+
+    for prop in [0.1, 0.2, 0.3]:
+        selected_indices = np.random.choice(np.arange(y_train.shape[0]), np.round(y_train.shape[0] * prop).astype(int), replace = False)
+        y_train_exp = y_train.copy()
+        y_train_exp[selected_indices] = 1 - y_train_exp[selected_indices]
+    
+        # Plot the data
+        plt.figure(figsize = (10, 4))
+        plt.scatter(
+            x_train[:, 0],
+            x_train[:, 1],
+            c = y_train_exp,
+            cmap = 'Set1'
+        )
+        plt.title('Training Data, Experiment 4', fontsize = 'xx-large')
+        plt.xlabel('Feature 1', fontsize = 'x-large')
+        plt.ylabel('Feature 2', fontsize = 'x-large')
+        plt.xticks(fontsize = 'large')
+        plt.yticks(fontsize = 'large')
+        plt.xlim(-0.5, 5.5)
+        plt.ylim(-0.5, 20.5)
+        plt.savefig('exp4/exp_4_training.png')
+
+        # Train the models
+        lrct = LRCTree(max_depth = 1, highest_degree = 3, n_bins = 20).fit(x_train, y_train_exp)
+        cart = DecisionTreeClassifier(max_depth = 1).fit(x_train, y_train_exp)
+        oc1 = ObliqueTree(splitter = 'oc1')
+        oc1.fit(x_train, y_train_exp)
+
+        # FIX BELOW HERE
         
-    # Plot the data
-    plt.figure(figsize = (10, 4))
-    plt.scatter(
-        x_train[:, 0],
-        x_train[:, 1],
-        c = y_train,
-        cmap = 'Set1'
-    )
-    plt.title('Training Data, Experiment 4', fontsize = 'xx-large')
-    plt.xlabel('Feature 1', fontsize = 'x-large')
-    plt.ylabel('Feature 2', fontsize = 'x-large')
-    plt.xticks(fontsize = 'large')
-    plt.yticks(fontsize = 'large')
-    plt.xlim(-0.5, 5.5)
-    plt.ylim(-0.5, 20.5)
-    plt.savefig('exp4/exp_4_training.png')
-
-    # Train the models
-    lrct = LRCTree(max_depth = 1, highest_degree = 3, n_bins = 20).fit(x_train, y_train)
-    cart = DecisionTreeClassifier(max_depth = 1).fit(x_train, y_train)
-    oc1 = ObliqueTree(splitter = 'oc1')
-    oc1.fit(x_train, y_train)
-
     # Print results
     print('LRCT Results')
     print('\n')
