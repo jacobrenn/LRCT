26cfc7c16ce56ce9a5edd6d05759c45d778d4aa7
Jacob Renn <jwrenn4@outlook.com>
2021-02-11 18:41:59 +0000

    updated plot and experiment script to get lrct gridsearch working

--- experiments/set_9_checkerboard.py
+++ experiments/set_9_checkerboard.py
@@ -14,6 +14,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from sklearn.model_selection import PredefinedSplit, GridSearchCV
 
 if __name__ == '__main__':
     # Generate the ata, domain for x0 and x1 is [0, 10]
@@ -24,14 +25,16 @@ if __name__ == '__main__':
 
     # Create train, val, test sets
     x_train_val, x_test, y_train_val, y_test = train_test_split(two_var_x, target, test_size = 0.3, random_state = 133523)
-    x_train, x_val, y_train, y_val = train_test_split(x_train_val, y_train_val, test_size = 0.4, random_state = 89342)
-
+    val_indices = np.random.choice(x_train_val.shape[0], int(x_train_val.shape[0] * 0.4), replace = False)
+    train_val_fold = [0 if i in val_indices else -1 for i in range(x_train_val.shape[0])]
+    ps = PredefinedSplit(test_fold = train_val_fold)
+    
     # Plot the data
     plt.figure(figsize = (10, 4))
     plt.scatter(
-        x_train[:, 0],
-        x_train[:, 1],
-        c = y_train,
+        x_train_val[train_val_fold == -1][:, 0],
+        x_train_val[train_val_fold == -1][:, 1],
+        c = y_train_val[train_val_fold == -1],
         cmap = 'Set1'
     )
     plt.title('Training Data, Experiment 9', fontsize = 'xx-large')
@@ -42,3 +45,22 @@ if __name__ == '__main__':
     plt.xlim(-0.5, 10.5)
     plt.ylim(-0.5, 10.5)
     plt.savefig('exp9/exp_9_training.png')
+
+    
+    lrct_params = {
+        'max_depth': range(8, 12),
+        'min_samples_split': range(5, 1, -1),
+        'min_samples_leaf': range(5, 1, -1),
+        'highest_degree': range(1, 3),
+        'n_bins': [10, 20]
+    }
+
+    lrct_searcher = GridSearchCV(
+        LRCTree(),
+        lrct_params,
+        n_jobs = -1,
+        verbose = 3,
+        cv = ps
+    ).fit(x_train_val, y_train_val)
+
+    model_report(lrct_searcher, x_test, y_test)
