42b0f7ac23a254dcf48e4072e9b0be062dd89f2a
Jacob Renn <jwrenn4@outlook.com>
2021-02-16 13:43:32 +0000

    adding neural net capabilities to evaluation function

--- experiments/exp_utils.py
+++ experiments/exp_utils.py
@@ -1,12 +1,26 @@
 from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score, classification_report
 
 def model_report(
-    model,
-    x_test,
-    y_test,
-    binary = True
+        model,
+        x_test,
+        y_test,
+        binary = True,
+        neural_net = False
 ):
-    preds = model.predict(x_test)
+    if neural_net:
+        probs = model.predict(x_test)
+        if binary:
+            probs = probs.flatten()
+            preds = (probs >= 0.5).astype(int)
+        else:
+            preds = probs.argmax(axis = 1)
+
+    else:
+        preds = model.predict(x_test)
+        try:
+            probs = model.predict_proba(x_test)[:, 1]
+        except:
+            pass
     
     acc = accuracy_score(y_test, preds)
     conf_mat = confusion_matrix(y_test, preds)
@@ -16,7 +30,6 @@ def model_report(
 
     if binary:
         try:
-            probs = model.predict_proba(x_test)[:, 1]
             auc = roc_auc_score(y_test, probs)
             print(f'AUC: {auc}')
         except:
